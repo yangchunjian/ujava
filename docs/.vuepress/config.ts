4e1ceef02518eb24b6ddc9ff8b008cbef98ce165
yangchunjian <1091938307@qq.com>
2023-09-21 17:17:40 +0800

    设计模式

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -52,12 +52,6 @@ export default defineUserConfig({
   plugins: [
     searchPlugin({
       // https://v2.vuepress.vuejs.org/zh/reference/plugin/search.html
-      // 排除首页
-      isSearchable: (page) => page.path !== "/",
-      maxSuggestions: 10,
-      hotKeys: ["s", "/"],
-      // 用于在页面的搜索索引中添加额外字段
-      getExtraFields: () => [],
       locales: {
         "/": {
           placeholder: "搜索",
