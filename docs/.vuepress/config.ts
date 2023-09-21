408d8142940ef37be31ede84906a896c99b363bc
yangchunjian <1091938307@qq.com>
2023-09-21 18:09:16 +0800

    设计模式

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,6 +1,6 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
-import {searchPlugin} from "@vuepress/plugin-search";
+// import { searchProPlugin } from 'vuepress-plugin-search-pro';
 
 export default defineUserConfig({
 
@@ -50,14 +50,22 @@ export default defineUserConfig({
   theme,
 
   plugins: [
-    searchPlugin({
-      // https://v2.vuepress.vuejs.org/zh/reference/plugin/search.html
-      locales: {
-        "/": {
-          placeholder: "搜索",
-        },
-      },
-    }),
+
+    // searchProPlugin({
+    //   // 索引全部内容
+    //   indexContent: true,
+    //   // 为分类和标签添加索引
+    //   customFields: [
+    //     {
+    //       getter: (page) => page.frontmatter.category,
+    //       formatter: "分类：$content",
+    //     },
+    //     {
+    //       getter: (page) => page.frontmatter.tag,
+    //       formatter: "标签：$content",
+    //     },
+    //   ],
+    // }),
     // searchProPlugin({
     //   indexContent: true,
     //   indexOptions: {
