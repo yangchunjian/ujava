6cc4e5f06465ffb0d218510c4c5f4d3f666a4870
yangchunjian <1091938307@qq.com>
2023-09-26 09:58:20 +0000

    Deploying to gh-pages from @ yangchunjian/ujava@4d2a2dac3298993000ed9b279343d07b68540204 🚀

---
+++ assets/index.html-a283b242.js
@@ -0,0 +1,33 @@
+import{_ as e}from"./plugin-vue_export-helper-c27b6911.js";import{o as t,c as a,e as r}from"./app-4971bf6b.js";const n={},d=r(`<h2 id="设计模式的六大原则" tabindex="-1"><a class="header-anchor" href="#设计模式的六大原则" aria-hidden="true">#</a> 设计模式的六大原则</h2><h3 id="一、创建型模式" tabindex="-1"><a class="header-anchor" href="#一、创建型模式" aria-hidden="true">#</a> 一、创建型模式</h3><pre><code>1、单例模式（Singleton Pattern）
+1）饿汉式
+2）懒汉式，双检锁
+3）静态内部类
+4）枚举
+2、原型模式（Prototype Pattern）
+3、工厂模式（Factory Pattern）
+4、抽象工厂模式（Abstract Factory Pattern）
+5、建造者模式（Builder Pattern）
+</code></pre><h3 id="二、结构型模式" tabindex="-1"><a class="header-anchor" href="#二、结构型模式" aria-hidden="true">#</a> 二、结构型模式</h3><pre><code>1、适配器模式（Adapter Pattern）
+2、组合模式（Composite Pattern）
+3、装饰器模式（Decorator Pattern）
+4、外观模式（Facade Pattern）
+5、享元模式（Flyweight Pattern）
+6、代理模式（Proxy Pattern）
+1）静态代理
+2）jdk动态代理
+3）cglib动态代理
+7、桥接模式（Bridge Pattern）
+</code></pre><h3 id="三、行为型模式" tabindex="-1"><a class="header-anchor" href="#三、行为型模式" aria-hidden="true">#</a> 三、行为型模式</h3><pre><code>1、责任链模式（Chain of Responsibility Pattern）
+2、命令模式（Command Pattern）
+3、解释器模式（Interpreter Pattern）
+4、迭代器模式（Iterator Pattern）
+5、观察者模式（Observer Pattern）
+</code></pre><h2 id="java自带观察者模式介绍" tabindex="-1"><a class="header-anchor" href="#java自带观察者模式介绍" aria-hidden="true">#</a> Java自带观察者模式介绍</h2><pre><code>6、状态模式（State Pattern）
+7、模板模式（Template Pattern）
+8、中介者模式（Mediator Pattern）
+9、备忘录模式（Memento Pattern）
+10、访问者模式（Visitor Pattern）
+11、策略模式（Strategy Pattern）
+</code></pre><h2 id="其他" tabindex="-1"><a class="header-anchor" href="#其他" aria-hidden="true">#</a> 其他</h2><pre><code>1、过滤器模式（Filter Pattern）
+2、空对象模式（Null Object Pattern）
+</code></pre>`,11),o=[d];function i(c,h){return t(),a("div",null,o)}const l=e(n,[["render",i],["__file","index.html.vue"]]);export{l as default};
