a96cc1572624465a8f6ba453572b157ea113b550
yangchunjian <1091938307@qq.com>
2023-11-15 16:17:57 +0800

    设计考量

--- docs/.vuepress/sidebar/index.ts
+++ docs/.vuepress/sidebar/index.ts
@@ -148,6 +148,7 @@ export default sidebar({
                 "cache",
                 "faulttolerant",
                 "databaseindex",
+                "consider",
             ],
         },
         {
