0e8bf63c2ea0f81f99bf7079fb174f2aba022e88
yangchunjian <1091938307@qq.com>
2024-02-23 15:12:07 +0800

    更新内容

--- docs/.vuepress/sidebar/index.ts
+++ docs/.vuepress/sidebar/index.ts
@@ -167,6 +167,7 @@ export default sidebar({
                 "modelrelation",
                 "reactor",
                 "webflux",
+                "modelchange",
             ],
         },
         {
