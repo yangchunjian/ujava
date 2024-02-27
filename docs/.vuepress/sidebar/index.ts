43e304c415628d0e1162e6acf21ad79ec7af3e36
yangchunjian <1091938307@qq.com>
2024-02-27 13:50:08 +0800

    更新内容

--- docs/.vuepress/sidebar/index.ts
+++ docs/.vuepress/sidebar/index.ts
@@ -170,6 +170,7 @@ export default sidebar({
                 "modelchange",
                 "serverless",
                 "springcloud",
+                "devops",
             ],
         },
         {
