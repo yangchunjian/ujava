0de96c61f5f9936d759621d395d8261a4583718f
yangchunjian <1091938307@qq.com>
2024-03-07 16:46:07 +0800

    更新内容

---
+++ ujava-web/src/main/java/cn/ujava/web/UjavaWebApplication.java
@@ -0,0 +1,13 @@
+package cn.ujava.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UjavaWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UjavaWebApplication.class, args);
+    }
+
+}
---
+++ ujava-web/src/test/java/cn/ujava/web/UjavaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ujava.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UjavaWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
