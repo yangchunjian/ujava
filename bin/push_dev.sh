81973ad623227d82840ed2ce9ed346c27dbe8041
yangchunjian <1091938307@qq.com>
2024-09-08 00:55:21 +0800

    更新内容

--- bin/push_dev.sh
+++ bin/push_dev.sh
@@ -5,5 +5,4 @@ git checkout v7-dev
 
 echo -e "\033[32mPush to origin v7-dev\033[0m"
 git push origin v7-dev
-echo -e "\033[32mPush to osc v7-dev\033[0m"
-git push osc v7-dev
+
--- bin/push_main.sh
+++ bin/push_main.sh
@@ -8,5 +8,4 @@ git merge v7-dev -m 'Prepare release'
 
 echo -e "\033[32mPush to origin v7-main\033[0m"
 git push origin v7-main
-echo -e "\033[32mPush to osc v7-main\033[0m"
-git push osc v7-main
+
