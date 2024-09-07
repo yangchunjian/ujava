d6c9e1f3d53f9286bf83806770e59229f9023efc
yangchunjian <1091938307@qq.com>
2024-09-07 11:13:04 +0800

    更新内容

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -60,6 +60,11 @@ export default defineUserConfig({
         __dirname,
         "./components/NormalPage.vue",
     ),
+    // 你可以在这里将别名定向到自己的组件
+    "@theme-hope/components/CommonWrapper": path.resolve(
+        __dirname,
+        "./components/CommonWrapper.vue",
+    ),
   },
 
   plugins: [
