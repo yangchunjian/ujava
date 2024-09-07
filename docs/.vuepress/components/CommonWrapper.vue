427f72d6b852e87008940c37735fd762c751aea0
yangchunjian <1091938307@qq.com>
2024-09-07 17:46:07 +0800

    更新内容

--- docs/.vuepress/components/CommonWrapper.vue
+++ docs/.vuepress/components/CommonWrapper.vue
@@ -1,13 +1,29 @@
+<script setup lang="ts">
+import CommonWrapper from "vuepress-theme-hope/components/CommonWrapper.js";
+const frontmatter = usePageFrontmatter<ThemePageFrontmatter>();
+const page = usePageData();
+import HomePage from "@theme-hope/components/HomePage";
+
+import type { ThemePageFrontmatter } from "vuepress-theme-hope";
+import { usePageData, usePageFrontmatter } from "vuepress/client";
+import NormalPage from "./NormalPage.vue";
+import FadeSlideY from "@theme-hope/components/transitions/FadeSlideY";
+
+</script>
+
 <template>
-  <HopeCommonWrapper>
+  <CommonWrapper>
+    <template #default>
+      <HomePage v-if="frontmatter.home" />
+      <FadeSlideY v-else>
+        <NormalPage :key="page.path" />
+      </FadeSlideY>
+    </template>
     <!-- 使用 sidebarTop 插槽引入组件 -->
-    <template #sidebarTop>
+    <template  v-if="!frontmatter.home"  #sidebarTop>
       <div style="text-align: center">
         <img src="/logo/gzh.png"/>
       </div>
     </template>
-  </HopeCommonWrapper>
+  </CommonWrapper>
 </template>
-<script setup lang="ts">
-import HopeCommonWrapper from "vuepress-theme-hope/components/CommonWrapper.js";
-</script>
