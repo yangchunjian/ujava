1bc77a6f05361839b39e58654ba5b41ddc1d149c
yangchunjian <1091938307@qq.com>
2024-09-07 10:59:27 +0800

    更新内容

---
+++ docs/.vuepress/components/CommonWrapper.vue
@@ -0,0 +1,13 @@
+<template>
+  <CommonWrapperPage>
+    <!-- 使用 contentAfter 插槽引入组件 -->
+    <template #sidebarTop>
+      <div style="text-align: center">
+        <img src="/logo/gzh.png"/>
+      </div>
+    </template>
+  </CommonWrapperPage>
+</template>
+<script setup lang="ts">
+import CommonWrapperPage from "vuepress-theme-hope/components/CommonWrapper.js";
+</script>
